b5e1fe8bf3b4b324d65fc1824cc3b418205e7822
Raheem <zahoorahmad.khanvardag@gmail.com>
2026-06-30 15:52:14 +0500

    Added Register schema and installed bcrypt

--- schemas/userschema.js
+++ schemas/userschema.js
@@ -4,5 +4,9 @@ export const userSchema = z.object({
   name: z.string().min(1, "Name is required").regex(/^[A-Za-z\s]+$/, "Name must only contain letters"),
   email: z.string().email("Invalid email format"),
 });
-
+export const registerSchema = z.object({
+  name: z.string().min(1, "Name is required").regex(/^[A-Za-z\s]+$/, "Name must only contain letters"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
 export const updateUserSchema = userSchema.partial();
